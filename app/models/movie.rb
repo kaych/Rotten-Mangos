23daa02214ef98c12caa31b794df3e8da982f073
Kay Cheng <puiki.cheng@gmail.com>
2016-02-01 23:55:44 +0000

    Add validations for movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,19 @@
 class Movie < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :director, presence: true
+  validates :runtime_in_minutes, numericality: { only_integer: true }
+  validates :description, presence: true
+  validates :poster_image_url, presence: true
+  validates :release_date, presence: true
+  validate :release_date_is_in_the_past 
+
+  protected 
+
+  def release_date_is_in_the_past
+    if release_date.present? 
+      errors.add(:release_date, "This needs to be in the past! Silly goose!") if release_date > Date.today
+    end
+  end
+
 end
