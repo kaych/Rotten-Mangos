f1841b51397d72a9c3e976cb1d28bcd2e795023b
Kay Cheng <puiki.cheng@gmail.com>
2016-02-02 23:25:30 +0000

    Add admin field to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202202551) do
+ActiveRecord::Schema.define(version: 20160202232255) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20160202202551) do
     t.datetime "updated_at"
     t.string   "firstname"
     t.string   "lastname"
+    t.boolean  "admin",           default: false
   end
 
 end
