0002be47568120b1885fa6ca2d8eee902bedac51
Kay Cheng <puiki.cheng@gmail.com>
2016-02-02 01:52:31 +0000

    Generate users controller and users/new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 RottenMangosApp::Application.routes.draw do
 
+  get "users/new"
+  get "users/create"
   root to: 'movies#index'
   resources :movies
+  resources :users, only: [:new, :create]
   
 end
 
