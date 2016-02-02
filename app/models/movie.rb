d0b815cdaad774ad232be79d8f16b002896eef7a
Kay Cheng <puiki.cheng@gmail.com>
2016-02-02 04:10:42 +0000

    Create average review, with refactoring

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -10,6 +10,14 @@ class Movie < ActiveRecord::Base
   validates :release_date, presence: true
   validate :release_date_is_in_the_past 
 
+  def review_average
+    if reviews.size > 0
+      reviews.sum(:rating_out_of_ten)/reviews.size
+    else
+      "(There are no reviews for this movie yet!)"
+    end
+  end
+
   protected 
 
   def release_date_is_in_the_past
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ActiveRecord::Base
   validates :lastname, presence: true
   validates :password, length: { in: 6..20 }, on: :create
 
+  def full_name
+    "#{firstname} #{lastname}"
+  end
+
 end
