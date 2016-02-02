0a97e8612e883d4b1f4ee6b9976dc4300c7bb352
Kay Cheng <puiki.cheng@gmail.com>
2016-02-02 03:22:40 +0000

    Create review migration and model, with validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ActiveRecord::Base
 
+  has_many :reviews
+
   validates :title, presence: true
   validates :director, presence: true
   validates :runtime_in_minutes, numericality: { only_integer: true }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  has_many :reviews
+
   has_secure_password
 
   validates :email, presence: true
