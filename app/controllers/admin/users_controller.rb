724ea086c71c172822d5ccc3ac65743c8596a0a9
Kay Cheng <puiki.cheng@gmail.com>
2016-02-03 06:30:57 +0000

    Add admin user creation functionality and related forms

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,8 +6,49 @@ class Admin::UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(admin_user_params)
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @user = User.new(admin_user_params)
+
+    if @user.save
+      redirect_to admin_users_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
+    flag[:alert] = "That user is a goner! Byeeee"
+  end
+
+  protected
+
+  def admin_user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :admin)
   end
 
 end
