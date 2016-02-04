d6743cb6bb7d3a8b28493ecddf49bde7d88b3903
Kay Cheng <puiki.cheng@gmail.com>
2016-02-04 02:20:48 +0000

    Integrate materialize folders and appropriate formatting

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module RottenMangosApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.paths << Rails.root.join("app", "assets", "font")
+    config.assets.precompile += %w( .svg .eot .woff .woff2 .ttf)
   end
 end
